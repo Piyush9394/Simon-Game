d63939985eb484fc20306f22335b4f474496ce41
Piyush9394 <piyushsethi9394@gmail.com>
2025-07-02 18:43:03 +0530

    game.js

--- game.js
+++ game.js
@@ -1,54 +1,23 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
 var level = 0;
 var speed = 900; // Initial game speed
-let touchStarted = false;
 
-$("#level-title").on("touchstart click", function (e) {
-    if (!started && !touchStarted) {
-        touchStarted = true;
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
-    }
-});
-
-});
+$(document).ready(function () {
+  // Start the game on title tap or click
+  $("#level-title").on("click touchstart", function () {
     if (!started) {
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
     }
-});
+  });
 
-function nextSequence() {
-    userClickedPattern = [];
-    level++;
-    $("#level-title").text("Level " + level);
-
-    if (level > 5) {
-        speed = 700; // Increase game speed after level 5
-    }
-    if (level > 10) {
-        speed = 500; // Further increase after level 10
-    }
-    if (level > 15){
-        speed = 300; // Further increase after level 15
-    }
-
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour);
-}
-
-$(".btn").click(function () {
+  // Handle user button click
+  $(".btn").on("click touchstart", function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
@@ -56,43 +25,64 @@ $(".btn").click(function () {
     animatePress(userChosenColour);
 
     checkAnswer(userClickedPattern.length - 1);
+  });
 });
 
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  if (level > 5) speed = 700;
+  if (level > 10) speed = 500;
+  if (level > 15) speed = 300;
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
 function checkAnswer(currentLevel) {
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function () {
-                nextSequence();
-            }, speed);
-        }
-    } else {
-        playSound("wrong");
-        $("body").addClass("game-over");
-        $("#level-title").text("Game Over, Press Any Key to Restart");
-
-        setTimeout(function () {
-            $("body").removeClass("game-over");
-        }, 200);
-
-        startOver();
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, speed);
     }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Tap to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
 }
 
 function playSound(name) {
-    var soundToBePlayed = new Audio("sounds/" + name + ".mp3");
-    soundToBePlayed.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
 function animatePress(currentColour) {
-    $("#" + currentColour).addClass("pressed");
-    setTimeout(function () {
-        $("#" + currentColour).removeClass("pressed");
-    }, 100);
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
 
 function startOver() {
-    level = 0;
-    gamePattern = [];
-    speed = 900; // Reset speed after game over
-    started = false;
+  level = 0;
+  gamePattern = [];
+  speed = 900;
+  started = false;
 }
